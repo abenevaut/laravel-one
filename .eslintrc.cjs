c511d5f49c722f5c1f1a38b7f1a095c335414dba
abenevaut <me@abenevaut.dev>
2024-11-09 11:26:00 +0000

    Update master to output generated at

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended'
+  ],
+  ignorePatterns: [
+    '.storybook',
+    'dist',
+    '.eslintrc.cjs',
+    'tailwind.config.js',
+    'vite.config.js'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react/jsx-no-target-blank': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+}
